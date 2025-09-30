ba64981b2fdb15f395ddba0ead627b884ae9c6f4
salaheddine mjydila <salaheddine.codes@gmail.com>
2025-09-30 02:21:54 +0100

    add meta name content to login and register pages by react helmet

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -10,69 +10,69 @@ import PageTitle from '../components/ui/PageTitle';
 const Login = () => {
   return (
     <>
-      <PageTitle title='Se connecter' />
+      <PageTitle title='Se connecter' content='Connectez-vous à votre compte pour accéder à la plateforme de gestion des ventes dentaires.' />
       <div className='container h-dvh grid grid-cols-1 lg:grid-cols-2 lg:gap-4 py-5'>
-        <div className='flex flex-col'>
-          <Link
-            to='/'
-            className='mb-auto mx-auto lg:mx-0'
-          >
-            <Image
-              src='images/logo.png'
-              alt='logo'
-              width='w-26'
-            />
-          </Link>
-          <div className='flex flex-col gap-2 w-full max-w-[450px] mx-auto'>
-            <h1 className='text-3xl font-bold text-center'>Se connecter</h1>
-            <p className='text-center text-gray-400'>
-              Veuillez entrer vos identifiants pour accéder à votre compte.
-            </p>
-            <form className='mt-4 grid grid-cols-1 gap-4'>
-              <TextField
-                label='Email'
-                name='email'
-                placeholder='Entrez votre email'
-                type='email'
-              />
-              <TextField
-                label='Mot de passe'
-                name='password'
-                placeholder='Entrez votre mot de passe'
-                type='password'
-              />
-              <Link
-                to='/login/reset-link'
-                className='text-sm text-right font-medium hover:text-blue-600 hover:underline'
-              >
-                Mot de passe oublié ?
-              </Link>
-              <Button
-                label='Se connecter'
-                type='submit'
-                classes='w-full mt-4'
-              />
-            </form>
-            <p className='text-center mt-2 text-sm'>
-              Vous n'avez pas de compte ?{' '}
-              <Link
-                to='/register'
-                className='text-blue-600 hover:underline inline-block'
-              >
-                Créez un compte
-              </Link>
-            </p>
-          </div>
-          <p className='mt-auto mx-auto lg:mx-0 text-sm text-gray-500'>
-            &copy; {new Date().getFullYear()} salahheddine mjydila. Tout droit
-            réservé
-          </p>
-        </div>
+      <div className='flex flex-col'>
+        <Link
+        to='/'
+        className='mb-auto mx-auto lg:mx-0'
+        >
         <Image
-          src='images/login-banner.jpg'
-          alt='banner'
-          classes='hidden lg:block rounded-lg'
+          src='images/logo.png'
+          alt='logo'
+          width='w-26'
         />
+        </Link>
+        <div className='flex flex-col gap-2 w-full max-w-[450px] mx-auto'>
+        <h1 className='text-3xl font-bold text-center'>Se connecter</h1>
+        <p className='text-center text-gray-400'>
+          Veuillez entrer vos identifiants pour accéder à votre compte.
+        </p>
+        <form className='mt-4 grid grid-cols-1 gap-4'>
+          <TextField
+          label='Email'
+          name='email'
+          placeholder='Entrez votre email'
+          type='email'
+          />
+          <TextField
+          label='Mot de passe'
+          name='password'
+          placeholder='Entrez votre mot de passe'
+          type='password'
+          />
+          <Link
+          to='/login/reset-link'
+          className='text-sm text-right font-medium hover:text-blue-600 hover:underline'
+          >
+          Mot de passe oublié ?
+          </Link>
+          <Button
+          label='Se connecter'
+          type='submit'
+          classes='w-full mt-4'
+          />
+        </form>
+        <p className='text-center mt-2 text-sm'>
+          Vous n'avez pas de compte ?{' '}
+          <Link
+          to='/register'
+          className='text-blue-600 hover:underline inline-block'
+          >
+          Créez un compte
+          </Link>
+        </p>
+        </div>
+        <p className='mt-auto mx-auto lg:mx-0 text-sm text-gray-500'>
+        &copy; {new Date().getFullYear()} salahheddine mjydila. Tout droit
+        réservé
+        </p>
+      </div>
+      <Image
+        src='images/login-banner.jpg'
+        alt='banner'
+        classes='hidden lg:block rounded-lg'
+      />
       </div>
     </>
   );
