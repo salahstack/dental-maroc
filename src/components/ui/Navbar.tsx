d8836b0ac4cc281f2083432b554e680f3177c4aa
salaheddine mjydila <salaheddine.codes@gmail.com>
2025-12-05 20:00:01 +0100

    style: fix navbar ui

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -68,7 +68,7 @@ const Navbar: FC<NavbarProps> = ({
               </li>
             );
           })}
-          <li>
+          <li className='xl:hidden'>
             <Button
               to='/auth/register'
               classes='mt-4'
@@ -76,7 +76,7 @@ const Navbar: FC<NavbarProps> = ({
               Register
             </Button>
           </li>
-          <li>
+          <li className='xl:hidden'>
             <Button to='/auth/login' variant='text' classes='mt-4'>Login</Button>
           </li>
         </ul>
